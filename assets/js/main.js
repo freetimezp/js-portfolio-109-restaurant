2627deb04bc3fa6b6f2836467cea8bb1d18b4d42
freetimezp <kraftukrnet@ukr.net>
2024-08-07 20:28:07 +0300

    06. js-portfolio-109-restaurant. Rest menu section. Tabs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,47 @@ window.addEventListener("scroll", function () {
     } else {
         document.querySelector(".header").classList.remove("header-bg");
     }
-
 });
+
+
+//tabs
+function tabs(tabsSelector) {
+    const tabsContainer = document.querySelector(tabsSelector);
+    const tabsList = tabsContainer.querySelector(".tabs-list");
+
+    tabsList.addEventListener("click", (event) => {
+        const clickedTab = event.target.closest(".tabs-btn");
+
+        if (!clickedTab || clickedTab.classList.contains("active")) {
+            return;
+        }
+
+        switchTab(clickedTab);
+    });
+
+    function switchTab(clickedTab) {
+        const activeTab = tabsContainer.querySelector(".tabs-btn.active");
+        const activePanel = tabsContainer.querySelector(".tabs-panel.active");
+
+        activeTab.classList.remove("active");
+        activePanel.classList.remove("active");
+
+        clickedTab.classList.add("active");
+        const target = clickedTab.getAttribute("data-target");
+        tabsContainer.querySelector(target).classList.add("active");
+    }
+};
+
+tabs(".rest-menu-tabs");
+
+
+
+
+
+
+
+
+
 
 
 
