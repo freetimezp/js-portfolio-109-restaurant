5496fdd0f3ffc964f495504c991c0bed30ddf186
freetimezp <kraftukrnet@ukr.net>
2024-08-07 16:12:39 +0300

    02. js-portfolio-109-restaurant. Header section.

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+//header bg
+window.addEventListener("scroll", function () {
+    if (this.scrollY >= 40) {
+        document.querySelector(".header").classList.add("header-bg");
+    } else {
+        document.querySelector(".header").classList.remove("header-bg");
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
